1a4c52a2cf195008bdf5badd511a11a3bd73c8d6
anggernuramin <anggern514@gmail.com>
2023-11-02 17:43:24 +0700

    solve responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true, // Mengatur container menjadi center
+      },
+    },
   },
   plugins: [],
 }
